c771621a4b86afa4277cbabb03d5a18671ce02c4
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-10-10 15:16:49 -0500

    changes .env2

--- src/config/config.js
+++ src/config/config.js
@@ -20,7 +20,7 @@ module.exports = {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": process.env.DB_HOTS,
+    "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
     "dialect": "postgres",
     "logging": false,
