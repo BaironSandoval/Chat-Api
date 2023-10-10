9578c61de9ed641a47ebfad68c151c7a30885ebc
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-10-10 14:47:31 -0500

    changes .env

--- src/config/config.js
+++ src/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "production": {
+  production: {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
